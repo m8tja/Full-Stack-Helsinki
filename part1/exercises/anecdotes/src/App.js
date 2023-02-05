3e650ea7d43f28e979966d528be26e8c63cc8cff
m8tja <mc5620@student.uni-lj.si>
2023-02-05 14:18:27 +0100

    exercises 1.12.-1.14.

---
+++ part1/exercises/anecdotes/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Votes = ({votesArr, index}) => {
+
+  return (
+    <p>has {votesArr[index]} votes</p>
+  )
+}
+
+const Title = ({title}) => {
+  
+  return (
+    <h2>{title}</h2>
+  )
+}
+
+const Anecdote = ({anecdotesArr, index}) => {
+
+  return (
+    <p>{anecdotesArr[index]}</p>
+  )
+}
+
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [max, setMax] = useState(0)
+
+  const handleClick = () => {
+    
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const handleVote = () => {
+    
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+
+    const maxi = copy.indexOf(Math.max(...copy))
+    setMax(maxi)
+  }
+
+  return (
+    <div>
+      <Title title="Anecdote of the day" />
+      <Anecdote anecdotesArr={anecdotes} index={selected} />
+      <Votes votesArr={votes} index={selected} />
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <Title title="Anecdota with most votes" />
+      <Anecdote anecdotesArr={anecdotes} index={max} />
+      <Votes votesArr={votes} index={max} />
+    </div>    
+  );
+}
+
+export default App;
