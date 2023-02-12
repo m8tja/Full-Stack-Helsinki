0758ec85691aec03edd3babf9565640bf9f6e37c
m8tja <mc5620@student.uni-lj.si>
2023-02-12 18:08:59 +0100

    connecting the frontend with the backend

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method)
