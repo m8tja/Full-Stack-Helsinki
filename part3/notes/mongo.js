2458c2eba272267a9db3902cbb896071e339adea
m8tja <mc5620@student.uni-lj.si>
2023-02-15 23:40:03 +0100

    fetching objects from the database

--- part3/notes/mongo.js
+++ part3/notes/mongo.js
@@ -21,11 +21,20 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model("Note", noteSchema)
 
 const note = new Note({
-  content: "HTML is Easy",
+  content: "Mongoose makes things easy",
   important: true
 })
 
+/*
 note.save().then(result => {
   console.log("note saved!")
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
