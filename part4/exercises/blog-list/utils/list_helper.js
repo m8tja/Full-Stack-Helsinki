daa6988ae85d3297046d211913bdbf3cb5fc1b57
m8tja <mc5620@student.uni-lj.si>
2023-03-04 10:06:02 +0100

    exercise 4.4.

--- part4/exercises/blog-list/utils/list_helper.js
+++ part4/exercises/blog-list/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+
+  let sum = 0
+
+  for(let i = 0; i < blogPosts.length; i++) {
+    sum += blogPosts[i].likes
+  }
+
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
