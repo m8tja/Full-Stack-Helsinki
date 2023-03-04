aabda7618c0cccbd4fde4ea915f942d0a9b66b42
m8tja <mc5620@student.uni-lj.si>
2023-03-04 10:22:23 +0100

    exercise 4.5.

--- part4/exercises/blog-list/utils/list_helper.js
+++ part4/exercises/blog-list/utils/list_helper.js
@@ -2,18 +2,34 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogPosts) => {
+const totalLikes = (blogs) => {
 
   let sum = 0
 
-  for(let i = 0; i < blogPosts.length; i++) {
-    sum += blogPosts[i].likes
+  for(let i = 0; i < blogs.length; i++) {
+    sum += blogs[i].likes
   }
 
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+
+  let currentMax = 0
+  let maxIndex = 0
+
+  for(let i = 0; i < blogs.length; i++) {
+    if(blogs[i].likes > currentMax) {
+      maxIndex = i
+      currentMax = blogs[i].likes
+    }
+  }
+
+  return (blogs.length === 0 ? [] : blogs[maxIndex])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
