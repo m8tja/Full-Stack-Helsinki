2c721a9e980e8b23dd682a1609573bdcb4570242
m8tja <mc5620@student.uni-lj.si>
2023-03-05 11:29:02 +0100

    initializing the database before tests

--- part4/notes/tests/note_api.test.js
+++ part4/notes/tests/note_api.test.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
-
 const api = supertest(app)
+const Note = require("../models/note")
+
+const initialNotes = [
+  {
+    content: "Html is Easy",
+    important: false
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test("notes are returned as json", async () => {
   await api
@@ -14,13 +33,14 @@ test("notes are returned as json", async () => {
 test("there are two notes", async () => {
   const response = await api.get("/api/notes")
 
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test("the first note is about HTTP methods", async () => {
   const response = await api.get("/api/notes")
+  const contents = response.body.map(r => r.content)
 
-  expect(response.body[0].content).toBe("HTML is Easy")
+  expect(contents).toContain("Browser can execute only JavaScript")
 })
 
 afterAll(async () => {
