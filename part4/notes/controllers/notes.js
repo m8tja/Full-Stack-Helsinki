6b6ca4438c1d881faced1a0b23871fb2f70c793e
m8tja <mc5620@student.uni-lj.si>
2023-03-05 12:21:03 +0100

    more tests and refactoring backend

--- part4/notes/controllers/notes.js
+++ part4/notes/controllers/notes.js
@@ -6,17 +6,19 @@ notesRouter.get("/", async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get("/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if(note) {
-        response.json(note)
-      }
-      else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get("/:id", async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if(note) {
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
+  }
+  catch(exception) {
+    next(exception)
+  }
 })
 
 notesRouter.post("/", async (request, response, next) => {
@@ -51,12 +53,14 @@ notesRouter.put("/:id", (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.delete("/:id", (request, response, next) => {
-  Note.findByIdAndUpdate(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+  catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = notesRouter
