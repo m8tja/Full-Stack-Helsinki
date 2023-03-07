20a2a708fa467111ba00e2077869abcfbd7fce56
m8tja <mc5620@student.uni-lj.si>
2023-03-07 21:21:17 +0100

    exercise 4.10.

--- part4/exercises/blog-list/tests/blog_api.test.js
+++ part4/exercises/blog-list/tests/blog_api.test.js
@@ -30,6 +30,34 @@ test("blog has property id and not _id", async () => {
   expect(returnedBlogs.body[0].id).toBeDefined()
 })
 
+test("a blog can be added", async () => {
+  const newBlog = {
+    title: "The Clean Code Blog",
+    author: "Robert C. Martin",
+    url: "https://blog.cleancoder.com/uncle-bob/2017/03/16/DrCalvin.html",
+    likes: 4,
+  }
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.blogs.length + 1)
+
+    const title = blogsAtEnd.map(b => b.title)
+    expect(title).toContain(
+      "The Clean Code Blog"
+    )
+
+    const author = blogsAtEnd.map(b => b.author)
+    expect(author).toContain(
+      "Robert C. Martin"
+    )
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
