53d2fb0fbc665fd9e17f77922b3ae18db729d63f
m8tja <mc5620@student.uni-lj.si>
2023-03-10 23:38:02 +0100

    put request

--- part4/exercises/blog-list/controllers/blog.js
+++ part4/exercises/blog-list/controllers/blog.js
@@ -13,6 +13,20 @@ blogRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogRouter.put("/:id", async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).end()
+})
+
 blogRouter.delete("/:id", async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
