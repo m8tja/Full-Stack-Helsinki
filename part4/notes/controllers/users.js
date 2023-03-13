94725cf1688ab1837cadd68e37c45f2f1ba0b86a
m8tja <mc5620@student.uni-lj.si>
2023-03-13 20:53:04 +0100

    creating users

--- part4/notes/controllers/users.js
+++ part4/notes/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require("bcrypt")
 const usersRotuer = require("express").Router()
 const User = require("../models/user")
 
+usersRotuer.get("/", async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 usersRotuer.post("/", async (request, response) => {
   const { username, name, password } = request.body
 
