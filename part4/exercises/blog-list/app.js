d7543f707fb4d009639db9bd59b8d7f4e6ce3e20
m8tja <mc5620@student.uni-lj.si>
2023-03-17 19:33:57 +0100

    exercise 4.15.

--- part4/exercises/blog-list/app.js
+++ part4/exercises/blog-list/app.js
@@ -6,6 +6,7 @@ const blogRouter = require("./controllers/blog")
 const middleware = require("./utils/middleware")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
+const usersRotuer = require("./controllers/users")
 
 mongoose.set("strictQuery", false)
 
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogRouter)
+app.use("/api/users", usersRotuer)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part4/exercises/blog-list/controllers/users.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt")
+const usersRotuer = require("express").Router()
+const User = require("../models/user")
+
+usersRotuer.get("/", async (request, response) => {
+  const users = await User.find({})
+
+  response.json(users)
+})
+
+usersRotuer.post("/", async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username, 
+    name, 
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRotuer
