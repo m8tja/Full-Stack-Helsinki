3cad5ed8549d19c2fff86b9dd8a0af29602a649e
m8tja <mc5620@student.uni-lj.si>
2023-03-19 11:55:45 +0100

    exercise 4.17.

--- part4/exercises/blog-list/controllers/blog.js
+++ part4/exercises/blog-list/controllers/blog.js
@@ -1,25 +1,39 @@
 const blogRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findOne()
+  console.log(user)
+
+  const blog = new Blog ({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
 
   if(!blog.likes) {
     blog.likes = 0
   }
 
   if(!blog.url || !blog.title) {
-    response.status(400).end()
+    return response.status(400).end()
   }
-  else {
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  }
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
+  response.status(201).json(savedBlog)
 })
 
 blogRouter.put("/:id", async (request, response) => {
