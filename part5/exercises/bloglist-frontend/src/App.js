2815b4cc1a370b029dfa99c3564d3babdc2a02be
m8tja <mc5620@student.uni-lj.si>
2023-03-31 21:11:56 +0200

    exercise 5.5.

--- part5/exercises/bloglist-frontend/src/App.js
+++ part5/exercises/bloglist-frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [errorType, setErrorType] = useState("")
+  const [blogFormVisible, setblogFormVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -126,27 +127,34 @@ const App = () => {
     </div>
   )
 
+  const hideWhenVisible = { display: blogFormVisible ? "none" : "" }
+  const showWhenVisible = { display: blogFormVisible ? "" : "none" }
+
   const blogForm = () => (
     <div>
       <form onSubmit={handleLogout}>
         <p>{user.name} logged in <button type='submit'>Log out</button></p>
       </form>
-      <h2>Create new</h2>
-      <form onSubmit={addBlog}>
-        <div>
-        <div>
-          title:  
-          <input type="text" value={newBlogTitle} onChange={handleTitleChange}/>
-        </div>
-        <div>
-          author:  
-          <input type="text" value={newBlogAuthor} onChange={handleAuthorChange}/>
-        </div>
-          url:  
-          <input type="text" value={newBlogUrl} onChange={handleUrlChange}/>
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <button style={hideWhenVisible} onClick={() => setblogFormVisible(true)}>new blog</button>
+      <div style={showWhenVisible}>
+        <h2>Create new</h2>
+        <form onSubmit={addBlog}>
+          <div>
+          <div>
+            title:  
+            <input type="text" value={newBlogTitle} onChange={handleTitleChange}/>
+          </div>
+          <div>
+            author:  
+            <input type="text" value={newBlogAuthor} onChange={handleAuthorChange}/>
+          </div>
+            url:  
+            <input type="text" value={newBlogUrl} onChange={handleUrlChange}/>
+          </div>
+          <button type="submit">create</button>
+        </form>
+        <button onClick={() => setblogFormVisible(false)}>cancel</button>
+      </div>
       <h2>Blogs</h2>
       {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
