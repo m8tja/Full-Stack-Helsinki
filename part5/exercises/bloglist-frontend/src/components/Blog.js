d4dbd1d2f0969368a90dcba353faebcf3bb05688
m8tja <mc5620@student.uni-lj.si>
2023-04-03 19:16:29 +0200

    exercise 5.7.

--- part5/exercises/bloglist-frontend/src/components/Blog.js
+++ part5/exercises/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+ 
+  const [fullBlogVisible, setFullBlogVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: fullBlogVisible ? "none" : "" }
+  const showWhenVisible = { display: fullBlogVisible ? "" : "none" }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setFullBlogVisible(true)} style={hideWhenVisible}>view</button>
+      <button onClick={() => setFullBlogVisible(false)} style={showWhenVisible}>hide</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+      </div>
+      <div style={showWhenVisible}>
+        likes {blog.likes} <button>like</button>
+      </div>
+    </div> 
+  )
+}
 
 export default Blog
