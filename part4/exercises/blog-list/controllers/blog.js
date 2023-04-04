b201b4ced60ed18a19a7d8198d13e5c645cfa6d1
m8tja <mc5620@student.uni-lj.si>
2023-04-04 19:15:14 +0200

    exercise 5.8.

--- part4/exercises/blog-list/controllers/blog.js
+++ part4/exercises/blog-list/controllers/blog.js
@@ -38,6 +38,7 @@ blogRouter.post("/", async (request, response) => {
   }
 
   const savedBlog = await blog.save()
+  savedBlog.populate("user", { username: 1, name: 1 })
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
