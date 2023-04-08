36b2e253ccf86405c7ab29763cb4304568f0b1de
m8tja <mc5620@student.uni-lj.si>
2023-04-08 18:01:18 +0200

    testing using CSS selectors

--- part5/notes/src/components/Note.test.js
+++ part5/notes/src/components/Note.test.js
@@ -14,3 +14,17 @@ test("renders content", () => {
   const element = screen.getByText("Component testing is done with react-testing-library")
   expect(element).toBeDefined()
 })
+
+test("renders content - CSS test", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true
+  }
+
+  const { container } = render(<Note note={note} />)
+
+  const div = container.querySelector(".note")
+  expect(div).toHaveTextContent(
+    "Component testing is done with react-testing-library"
+  )
+})
