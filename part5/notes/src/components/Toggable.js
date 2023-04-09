1a5c60e4c92986b53527c3455e28b19a16c9838d
m8tja <mc5620@student.uni-lj.si>
2023-04-09 19:18:23 +0200

    tests for the toggable component

--- part5/notes/src/components/Toggable.js
+++ part5/notes/src/components/Toggable.js
@@ -22,7 +22,7 @@ const Toggable = forwardRef((props, refs) => {
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={showWhenVisible}>
+      <div style={showWhenVisible} className="toggableContent">
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
