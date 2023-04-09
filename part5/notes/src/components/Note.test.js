6bf0cc3ec0c6721f00b6cd0f21def6edc2dc103b
m8tja <mc5620@student.uni-lj.si>
2023-04-09 18:21:02 +0200

    debugging tests

--- part5/notes/src/components/Note.test.js
+++ part5/notes/src/components/Note.test.js
@@ -12,6 +12,8 @@ test("renders content", () => {
   render(<Note note={note} />)
 
   const element = screen.getByText("Component testing is done with react-testing-library")
+
+  screen.debug(element)
   expect(element).toBeDefined()
 })
 
