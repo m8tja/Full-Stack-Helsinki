7ca481adf13f968d51db2b1bd8bb9083f3a29481
m8tja <mc5620@student.uni-lj.si>
2023-04-10 19:40:28 +0200

    exercise 5.14.

--- part5/exercises/bloglist-frontend/src/components/Blog.test.js
+++ part5/exercises/bloglist-frontend/src/components/Blog.test.js
@@ -1,23 +1,23 @@
 import React from "react"
-import { render } from "@testing-library/react" //screen
+import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import Blog from "./Blog"
-//import userEvent from "@testing-library/user-event"
+import userEvent from "@testing-library/user-event"
 
-test("renders content - CSS test", () => {
-  const user = {
-    username: "tester",
-    name: "Test User"
-  }
+const user = {
+  username: "tester",
+  name: "Test User"
+}
 
-  const blog = {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    user: user
-  }
+const blog = {
+  title: "First class tests",
+  author: "Robert C. Martin",
+  url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+  likes: 10,
+  user: user
+}
 
+test("title and content are displayed, url and likes are hidden", () => {
   const { container } = render(<Blog blog={blog} user={user}/>)
 
   const divTitleAuthor = container.querySelector(".title-author")
@@ -28,4 +28,19 @@ test("renders content - CSS test", () => {
 
   const divLikes = container.querySelector(".likes")
   expect(divLikes).toHaveStyle("display: none")
+})
+
+test("after clicking the button, url and likes are displayed", async () => {
+  const user = userEvent.setup()
+
+  const { container } = render(<Blog blog={blog} user={user}/>)
+
+  const button = screen.getByText("view")
+  await user.click(button)
+
+  const divUrl = container.querySelector(".url")
+  expect(divUrl).toHaveStyle("display: block")
+
+  const divLikes = container.querySelector(".likes")
+  expect(divLikes).toHaveStyle("display: block")
 })
