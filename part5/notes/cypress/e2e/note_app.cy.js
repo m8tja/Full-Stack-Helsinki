8071778135dd131573d7c41f6505c72fb2a72dbc
m8tja <mc5620@student.uni-lj.si>
2023-04-13 20:28:23 +0200

    writing to a form

--- part5/notes/cypress/e2e/note_app.cy.js
+++ part5/notes/cypress/e2e/note_app.cy.js
@@ -1,7 +1,19 @@
 describe("Note app", function() {
-  it("front page can be opened", function() {
+  beforeEach(function() {
     cy.visit("http://localhost:3000")
+  })
+
+  it("front page can be opened", function() {
     cy.contains("Notes")
     cy.contains("Note app, Department of Computer Science, University of Helsinki 2023")
+  })
+
+  it("login form can be opened", function() {
+    cy.contains("log in").click()
+    cy.get("#username").type("muukkai") //cy.get("input:first").type("mluukkai")
+    cy.get("#password").type("salainen") //cy.get("input:last").type("salainen")
+    cy.get("#login-button").click()
+
+    cy.contains("Matti Luukkainen logged in")
   })
 })
