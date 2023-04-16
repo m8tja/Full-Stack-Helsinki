424f835af31ef9faf91fbf8030cdbde6831e04a9
m8tja <mc5620@student.uni-lj.si>
2023-04-16 11:23:46 +0200

    changing the importance of a note

--- part5/notes/cypress/e2e/note_app.cy.js
+++ part5/notes/cypress/e2e/note_app.cy.js
@@ -36,21 +36,31 @@ describe("Note app", function() {
       cy.contains("a note created by cypress")
     })
 
-    describe("and a note exists", function() {
+    describe("and several notes exist", function() {
       beforeEach(function() {
-        cy.createNote({
-          content: "another note cypress",
-          important: true
-        })
+        cy.createNote({ content: "another note cypress", important: true })
+        cy.createNote({ content: "second note", important: false })
+        cy.createNote({ content: "third note", important: false })
       })
 
       it("it can be made not important", function() {
-        cy.contains("another note cypress")
-          .contains("make not important")
+        cy.contains("another note cypress").parent().find("button").as("theButton")
+        cy.get("@theButton").click()
+        cy.get("@theButton").should("contain", "make important")
+      })
+
+      it("one of those can be made important", function() {
+        cy.contains("second note").parent().find("button").as("theButton")
+        cy.get("@theButton").click()
+        cy.get("@theButton").should("contain", "make not important")
+        /*
+        cy.contains("second note")
+          .contains("make important")
           .click()
 
-        cy.contains("another note cypress")
-          .contains("make important")
+        cy.contains("second note")
+          .contains("make not important")
+        */
       })
     })
   })
