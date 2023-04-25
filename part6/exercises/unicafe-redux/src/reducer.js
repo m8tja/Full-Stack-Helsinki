93909d68956645b8b7ebea684204c8106bdff88e
m8tja <mc5620@student.uni-lj.si>
2023-04-25 19:48:22 +0200

    exercise 6.2.

--- part6/exercises/unicafe-redux/src/reducer.js
+++ part6/exercises/unicafe-redux/src/reducer.js
@@ -5,7 +5,7 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  //console.log(action)
   let changedState = {}
   switch (action.type) {
     case 'GOOD':
