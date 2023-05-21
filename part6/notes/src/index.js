ed31aa453344f82cb36698ebf34eb572feb900c6
m8tja <mc5620@student.uni-lj.si>
2023-05-21 18:50:57 +0200

    redux toolkit

--- part6/notes/src/index.js
+++ part6/notes/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from "@reduxjs/toolkit"
 import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer';
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
